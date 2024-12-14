d44cb0a27c8ee6c2be52ab727a7cb5852b35892f
gornius <rafal.cisek121@gmail.com>
2024-12-14 20:15:46 +0100

    Day 11 - memoization and refactors to make it work for part 2

--- cmd/day11/main.go
+++ cmd/day11/main.go
@@ -9,73 +9,20 @@ type Stone struct {
 	Val uint64
 }
 
-func GetMapOfNumberOfStonesWithValAfterNBlinks(val uint64) map[int]int {
-	m := map[int]int{}
-	stones := []Stone{{val}}
-
-	for i := 1; i <= TotalNumberOfSteps; i++ {
-		fmt.Println("-- PRECALCULATING STEP", i, "FOR", val, "--")
-		stones = Blink(stones)
-		m[i] = len(stones)
-	}
-
-	return m
-}
-
-var MapOfMadeStonesForStone1 map[int]int = GetMapOfNumberOfStonesWithValAfterNBlinks(1)
-
-const TotalNumberOfSteps int = 25
+const TotalNumberOfSteps int = 75
 
 func main() {
-	// stones := []Stone{{125}, {17}}
 	stones := []Stone{{6563348}, {67}, {395}, {0}, {6}, {4425}, {89567}, {739318}}
-	// part1(stones)
-	part1Eager(stones)
+	solveProblem(stones)
 }
 
-func part1(stones []Stone) {
-	for i := 1; i <= TotalNumberOfSteps; i++ {
-		fmt.Println("=== BLINK", i, "===")
-		stones = Blink(stones)
-		fmt.Printf("len(stones): %v\n", len(stones))
-	}
-
-	fmt.Printf("len(stones): %v\n", len(stones))
-
-}
-
-func part1Eager(stones []Stone) {
-	var eagerNumberOfStones uint64 = 0
-
-	for i := 1; i <= TotalNumberOfSteps; i++ {
-		fmt.Println("=== BLINK", i, "===")
-		EagerProcessResults(&stones, &eagerNumberOfStones, i)
-		stones = Blink(stones)
-		fmt.Printf("len(stones): %v\n", len(stones))
-		fmt.Printf("eagerNumberOfStones: %v\n", eagerNumberOfStones)
-		fmt.Println("Total:", len(stones)+int(eagerNumberOfStones))
-	}
-}
-
-func EagerProcessResults(stones *[]Stone, quantityOfStones *uint64, currentStep int) {
-	for i, stone := range *stones {
-		if stone.Val == 1 {
-			remainingNumberOfSteps := TotalNumberOfSteps - currentStep + 1
-			*quantityOfStones += uint64(MapOfMadeStonesForStone1[remainingNumberOfSteps])
-			*stones = append((*stones)[:i], (*stones)[i+1:]...)
-		}
-	}
-}
-
-func Blink(stones []Stone) []Stone {
-	newStones := []Stone{}
-
+func solveProblem(stones []Stone) {
+	var total uint64 = 0
 	for _, stone := range stones {
-		stonesFromStone := ProcessStone(stone)
-		newStones = append(newStones, stonesFromStone...)
+		total += GetQuantityOfStonesProduced(int(stone.Val), TotalNumberOfSteps)
 	}
 
-	return newStones
+	fmt.Printf("total: %v\n", total)
 }
 
 func ProcessStone(stone Stone) []Stone {
@@ -95,4 +42,38 @@ func ProcessStone(stone Stone) []Stone {
 	}
 
 	return stones
+}
+
+type GetQuantityOfStonesProducedArgs struct {
+	StoneVar, NumberOfBlinks int
+}
+
+var GetQuantityOfStonesProducedMemos = map[GetQuantityOfStonesProducedArgs]uint64{}
+
+func GetQuantityOfStonesProduced(stoneVar int, numberOfBlinks int) uint64 {
+	memo, ok := GetQuantityOfStonesProducedMemos[GetQuantityOfStonesProducedArgs{stoneVar, numberOfBlinks}]
+	if !ok {
+		memo = GetQuantityOfStonesProducedProcessor(stoneVar, numberOfBlinks)
+		GetQuantityOfStonesProducedMemos[GetQuantityOfStonesProducedArgs{stoneVar, numberOfBlinks}] = memo
+	}
+
+	return memo
+}
+
+func GetQuantityOfStonesProducedProcessor(stoneVar int, numberOfBlinks int) uint64 {
+	if numberOfBlinks == 1 {
+		stoneVarStr := strconv.Itoa(stoneVar)
+		if len(stoneVarStr)%2 == 0 {
+			return 2
+		}
+		return 1
+	}
+
+	newStones := ProcessStone(Stone{uint64(stoneVar)})
+
+	var acc uint64 = 0
+	for _, newStone := range newStones {
+		acc += GetQuantityOfStonesProduced(int(newStone.Val), numberOfBlinks-1)
+	}
+	return acc
 }
